dd9ac584911168a8bc841558f351f8f7c4be338b
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2024-03-22 19:35:07 +0200

    Added comment to overlay the game's structure

--- ChessGame/ChessGame/Program.cs
+++ ChessGame/ChessGame/Program.cs
@@ -1,15 +1,25 @@
-﻿using TileMapEngine;
+﻿using Engine;
+
+namespace ChessGame;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        TileMap tileMap = new TileMap(6, 6);
-        foreach (var tile in tileMap)
-        {
-            if(tile.Position.X == 0) Console.WriteLine();
-            Console.Write(tile);
-        }
+        // Config the scene of the game:
+        // Create a new scene
+        // create a new 8*8 board with the black and white pattern
+        // config the visuals of the board (cell size, color palette, etc.)
         
+        // Config the game rules:
+        // Create the different chess pieces types
+        // determine the rules for each piece type
+        // determine the game's win condition
+        // determine the game's flow (turns and their logic)
+
+        // Config the players
+        // create 2 players
+        // determine a name and color to each player
+        // add to each players their 16 pieces
     }
 }
