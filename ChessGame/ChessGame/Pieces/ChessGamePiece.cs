9662bdba2f312ef5c8bf1b15cce6f4411229c9fe
BmanStudio <oribennoon@gmail.com>
2024-05-04 23:19:26 +0300

    Added more pieces classes into ChessGamePiece.cs

--- ChessGame/ChessGame/Pieces/ChessGamePiece.cs
+++ ChessGame/ChessGame/Pieces/ChessGamePiece.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using ConsoleRenderer.ConsoleRenderer;
 using Renderer.Rendering;
 using TileMapEngine.CoreEngine;
 using TileMapEngine.CoreEngine.TileObject;
@@ -14,5 +15,81 @@ public abstract class ChessGamePiece(ITileRenderer renderer, List<MovePattern> m
         renderer.Init(drawable, new Vector2(position2D.X, position2D.Y));
         
         GameManager.AddObjectToTileMap(this, position2D);
+    }
+}
+
+public class Pawn : ChessGamePiece
+{
+    private static ConsoleDrawableString _drawableString = new("P");
+    private static readonly List<MovePattern> MovePatterns = [
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Forward], false),
+    ];
+    
+    public Pawn(ITileRenderer renderer, Tile tile, ConsoleColor color) : base(renderer, MovePatterns, tile)
+    {
+        _drawableString.FgConsoleColor = color;
+        var position = tile.Position;
+        Init(renderer, _drawableString, position);
+    }
+}
+
+public class Bishop : ChessGamePiece
+{
+    private static ConsoleDrawableString _drawableString = new("B");
+    private static readonly List<MovePattern> MovePatterns = [
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.BackLeft], true),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.ForwardLeft], true),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.BackRight], true),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.ForwardRight], true),
+    ];
+    
+    public Bishop(ITileRenderer renderer, Tile tile, ConsoleColor color) : base(renderer, MovePatterns, tile)
+    {
+        _drawableString.FgConsoleColor = color;
+        var position = tile.Position;
+        Init(renderer, _drawableString, position);
+    }
+}
+
+public class Rook : ChessGamePiece
+{
+    private static ConsoleDrawableString _drawableString = new("R");
+    private static readonly List<MovePattern> MovePatterns = [
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Forward], true),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Back], true),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Right], true),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Left], true),
+    ];
+    
+    public Rook(ITileRenderer renderer, Tile tile, ConsoleColor color) : base(renderer, MovePatterns, tile)
+    {
+        _drawableString.FgConsoleColor = color;
+        var position = tile.Position;
+        Init(renderer, _drawableString, position);
+    }
+}
+
+public class Knight : ChessGamePiece
+{
+    private static ConsoleDrawableString _drawableString = new("N");
+    private static readonly List<MovePattern> MovePatterns = [
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Left, TileMapEngine.CoreEngine.TileObject.Movement.ForwardLeft], false),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Left, TileMapEngine.CoreEngine.TileObject.Movement.BackLeft], false),
+        
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Forward, TileMapEngine.CoreEngine.TileObject.Movement.ForwardLeft], false),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Forward, TileMapEngine.CoreEngine.TileObject.Movement.ForwardRight], false),
+        
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Right, TileMapEngine.CoreEngine.TileObject.Movement.ForwardRight], false),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Right, TileMapEngine.CoreEngine.TileObject.Movement.BackRight], false),
+        
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Back, TileMapEngine.CoreEngine.TileObject.Movement.BackLeft], false),
+        new MovePattern([TileMapEngine.CoreEngine.TileObject.Movement.Back, TileMapEngine.CoreEngine.TileObject.Movement.BackRight], false),
+    ];
+    
+    public Knight(ITileRenderer renderer, Tile tile, ConsoleColor color) : base(renderer, MovePatterns, tile)
+    {
+        _drawableString.FgConsoleColor = color;
+        var position = tile.Position;
+        Init(renderer, _drawableString, position);
     }
 }
--- ChessGame/ChessGame/Pieces/GamePiecesManager.cs
+++ ChessGame/ChessGame/Pieces/GamePiecesManager.cs
@@ -21,10 +21,22 @@ public class GamePiecesManager
             var pawn = new Pawn(new ConsoleTileRenderer(), tile25, ConsoleColor.Yellow);
         }
         
+        var tile12 = GameManager.TileMap?[1, 2];
+        if (tile12 != null)
+        {
+            var bishop = new Bishop(new ConsoleTileRenderer(), tile12, ConsoleColor.Yellow);
+        }
+        
+        var tile77 = GameManager.TileMap?[7, 7];
+        if (tile77 != null)
+        {
+            var rook = new Rook(new ConsoleTileRenderer(), tile77, ConsoleColor.Yellow);
+        }
+        
         var tile46 = GameManager.TileMap?[4, 6];
         if (tile46 != null)
         {
-            var bishop = new Bishop(new ConsoleTileRenderer(), tile46, ConsoleColor.Yellow);
+            var knight = new Knight(new ConsoleTileRenderer(), tile46, ConsoleColor.Yellow);
         }
 
         GameManager.RefreshGameViewport(true);
